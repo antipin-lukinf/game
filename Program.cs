d4b00b4c146aa43baf8d7394a9cf1697b88658e6
Drei4 <antipin-lukinf@mail.ru>
2022-08-09 14:17:24 +0300

    add case in game RPG

--- Program.cs
+++ Program.cs
@@ -1,36 +1,54 @@
-﻿class Program
+﻿class Hero
 {
-    static void SelectRase()
+    public string Name;
+    public string Sex;
+    public string Rase;
+    public string Class;
+    Dictionary<string, int> Skills = new Dictionary<string, int>()
+    {
+        {"Сила",0},
+        {"Удача",0},
+        {"Ловкость",0},
+        {"Харизма",0},
+        {"Эмпатия",0},
+        {"Красноречие",0},
+    };
+}
+
+
+
+
+class Program
+{
+    static void SelectRase(Hero myHero)
     {
         string[] namesRase = { "Люди", "Эльфы", "Гномы", "Орки", "Гоблины" };
         Console.WriteLine("Выбери свою расу");
         Select(namesRase);
-        /*for (int i = 0; i < namesRase.Length; i++)
-        {
-            Console.Write(i+1 + " - " + namesRase[i] + " ");
-        }
-        Console.Write(namesRase.Length + " - " + namesRase[namesRase.Length - 1] + ".");
-        Console.WriteLine(" ");*/
+       
         string RaseHero = namesRase[Convert.ToInt32(Console.ReadLine()) - 1];              
         Console.WriteLine("Вы выбрали Расу " + RaseHero);
+        myHero.Rase = RaseHero;
     }
 
-    static void SelectSex()
+    static void SelectSex(Hero myHero)
     {
         string[] namesSex = { "Женский", "Мужской" };
         Console.WriteLine("Выбери свой пол");
         Select(namesSex);
         string SexHero = namesSex[Convert.ToInt32(Console.ReadLine()) - 1];              
         Console.WriteLine("Вы выбрали пол " + SexHero);
+        myHero.Sex = SexHero;
     }
 
-     static void SelectClass()
+     static void SelectClass(Hero myHero)
     {
         string[] namesClass = { "Воин", "Разбойник", "Маг", "Вор" };
         Console.WriteLine("Выбери свой класс");
         Select(namesClass);
         string ClassHero = namesClass[Convert.ToInt32(Console.ReadLine()) - 1];              
         Console.WriteLine("Вы выбрали класс " + ClassHero);
+        myHero.Class = ClassHero;
     }
 
     static void Select(string[] words)
@@ -47,9 +65,22 @@
 
     static void Main(string[] args)
     {
-        SelectRase();
-        SelectSex();
-        SelectClass();
+        Hero myHero = new Hero();
+        Console.WriteLine("Как вас зовут?");
+        myHero.Name = Console.ReadLine();
+        SelectRase(myHero);
+        SelectSex(myHero);
+        SelectClass(myHero);
+
+        Console.WriteLine("Все наши хар-ки");
+        Console.WriteLine("Ваше имя " + myHero.Name);
+        Console.WriteLine("Ваш пол " + myHero.Sex);
+        Console.WriteLine("Ваша раса " + myHero.Rase);
+        Console.WriteLine("Ваш класс " + myHero.Class);
+
+
+
+
     }
 
 }
